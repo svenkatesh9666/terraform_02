0e788e7416fc16cafc0c79e3face77fb92f1f0f3
svenkatesh9666 <137752334+svenkatesh9666@users.noreply.github.com>
2023-08-24 10:40:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.64.0"
-    }
-  }
-}
-
 provider "aws" {
   # Configuration options
   region = "ap-south-1"
